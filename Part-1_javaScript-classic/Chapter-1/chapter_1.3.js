d66deb32f323f0a4b0a6f11ff6bb65b14828365b
fahimfaisaal <fahimfaisal.soikot@gmail.com>
2021-06-08 10:06:23 +0600

    feat: add 1.3 isEmpty function

--- Part-1_javaScript-classic/Chapter-1/chapter_1.3.js
+++ Part-1_javaScript-classic/Chapter-1/chapter_1.3.js
@@ -1,2 +1,14 @@
 //* Checking for an existing, Nonempty string
 
+const isEmpty = str => {
+    if (
+        (typeof str !== 'undefined' && str) &&
+        typeof str === 'string'
+    ) {
+        return false;
+    }
+
+    return true;
+}
+
+console.log(isEmpty('fahim'));
